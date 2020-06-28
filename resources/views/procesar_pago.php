d4b1249d3d514994c8c96ff83e71b41b64cd83e9
Cesh399 <Cesh399@users.noreply.github.com>
2020-06-28 15:43:09 -0400

    vista de procesar pago

--- resources/views/procesar_pago.php
+++ resources/views/procesar_pago.php
@@ -19,32 +19,68 @@ require 'navbar.php';
     Procesar pago
 </h2>
 <br>
-
-<section class="container-fluid">
-    <section class="row justify-content-md-center">
-        <section class="col-12 col-md-4">
-            <form class="form-container">
-                <div></div>
-            </form>
-        </section>
-    </section>
-</section>
-
-<div>
-    <div class="text-left">
-        <div>
-            <p>
-                Tipo de pago
-            </p>
-            <div class="form-check ">
-                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
-                <label class="form-check-label" for="inlineRadio1">Visa</label>
-            </div>
+<br>
+<div class="row">
+    <div class="col-md-6 text-right">
+        <p>
+            Tipo de pago
+        </p>
+        <br>
+        <p class="mt-3">
+            Numero de tarjeta
+        </p>
+        <br>
+        <p>
+            Fecha de vencimiento
+        </p>
+        <br>
+        <p>
+            CSC
+        </p>
+    </div>
+    <div class="col-md-3 text-left">
+        <div class="form-check for">
+            <input class="form-check-input mt-2" type="radio" name="visa_card" id="visa" value="visa">
+            <label class="form-check-label ml-3" for="inlineRadio1">Visa</label>
+            &nbsp
+            &nbsp
+            &nbsp
+            &nbsp
+            &nbsp
+            &nbsp
+            &nbsp
+            &nbsp
+            <img class="tab-icon" src="https://www.svgrepo.com/show/98426/visa-logo.svg" alt="icono de visa">
         </div>
         <div class="form-check ">
-            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
-            <label class="form-check-label" for="inlineRadio2">Mastercard</label>
+            <input class="form-check-input mt-2" type="radio" name="mastercard_card" id="mastercard" value="mastercard">
+            <label class="form-check-label ml-3" for="mastercard">Mastercard</label>
+            &nbsp
+            &nbsp
+            <img class="tab-icon" src="https://image.flaticon.com/icons/svg/38/38943.svg" alt="icono de visa">
         </div>
+        <br>
+        <div class="col-md-12 text-left">
+            <input class="form-control " type="text" placeholder="#">
+        </div>
+        <br>
+        <div class="col-md-8 text-left">
+            <input class="form-control mt-1" type="date" placeholder="#">
+        </div>
+        <br>
+        <div class="col-md-4 text-left">
+            <input class="form-control " type="text" placeholder="#">
+        </div>
+    </div>
+</div>
+<br>
+<br>
+<div class="row">
+    <div class="col-md-6 text-center">
+        <button type="button" class="btn btn-primary">Volver</button>
+    </div>
+    <div class="col-md-6 text-center">
+        <button type="button" class="btn btn-primary">Pagar</button>
     </div>
 </div>
 </body>
