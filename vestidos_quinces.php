d31e4dc00560ec90d5f90d89dc6f68bab5c5bf0b
Cesh399 <Cesh399@users.noreply.github.com>
2020-07-18 21:03:52 -0400

    clase de imagenes de disenador

--- vestidos_quinces.php
+++ vestidos_quinces.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="css/style.css">
     <script src="js/carrusel.js"></script>
     <style>
-        img {
+        .imagen_disenador {
             max-width: 25rem;
             height: 18rem;
         }
@@ -70,7 +70,7 @@ require 'navbar_quinces.php';
 <div class="row">
     <div class="col-lg-4 col-md-4 col-xs-4 thumb ml-4">
         <a class="thumbnail" href="#">
-            <img class="theme_img" src="https://www.latercera.com/resizer/82YKLqyp887B8VFEs8MZGh2HL9I=/900x600/smart/arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/BDZTFLTLB5FDJFW3OD2CI3VTQU.jpg">
+            <img class="theme_img imagen_disenador" src="https://www.latercera.com/resizer/82YKLqyp887B8VFEs8MZGh2HL9I=/900x600/smart/arc-anglerfish-arc2-prod-copesa.s3.amazonaws.com/public/BDZTFLTLB5FDJFW3OD2CI3VTQU.jpg">
         </a>
         <p class="text-center">
             Ozzy Osborne - Diseñador
@@ -83,19 +83,19 @@ require 'navbar_quinces.php';
     <div class="col-lg-7 col-md-7 ml-2">
         <div class="contenedor-carrusel">
             <div class="imagen-carrusel actual-carrusel">
-                <img src="https://www.vanguardiaveracruz.mx/wp-content/uploads/2020/01/ESTADOS-UNIDOS-Ozzy.jpg" />
+                <img class="imagen_disenador" src="https://www.vanguardiaveracruz.mx/wp-content/uploads/2020/01/ESTADOS-UNIDOS-Ozzy.jpg" />
             </div>
 
             <div class="imagen-carrusel">
-                <img src="https://media.vogue.mx/photos/5e2f2ee76b988c0008c7f964/master/w_2632,c_limit/Osbourne--Grammy-2020-.jpg" />
+                <img class="imagen_disenador" src="https://media.vogue.mx/photos/5e2f2ee76b988c0008c7f964/master/w_2632,c_limit/Osbourne--Grammy-2020-.jpg" />
             </div>
 
             <div class="imagen-carrusel">
-                <img src="https://townsquare.media/site/295/files/2019/03/ozzyGeezer.jpg" />
+                <img class="imagen_disenador" src="https://townsquare.media/site/295/files/2019/03/ozzyGeezer.jpg" />
             </div>
 
             <div class="imagen-carrusel">
-                <img src="https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1686,w_3000,x_0,y_0/dpr_1.5/c_limit,w_1600/fl_lossy,q_auto/v1588397111/200430-stern-ozzy-tease_b1wx4q" />
+                <img class="imagen_disenador" src="https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_1686,w_3000,x_0,y_0/dpr_1.5/c_limit,w_1600/fl_lossy,q_auto/v1588397111/200430-stern-ozzy-tease_b1wx4q" />
             </div>
 
 
@@ -116,7 +116,7 @@ require 'navbar_quinces.php';
 <div class="row">
     <div class="col-lg-4 col-md-4 col-xs-4 thumb ml-4">
         <a class="thumbnail" href="#">
-            <img class="theme_img" src="https://rockandblog.net/wp-content/uploads/2019/11/judas-priest-rob-halford-1.jpg">
+            <img class="theme_img imagen_disenador" src="https://rockandblog.net/wp-content/uploads/2019/11/judas-priest-rob-halford-1.jpg">
         </a>
         <p class="text-center">
             Rob Halford - Modista
@@ -129,19 +129,19 @@ require 'navbar_quinces.php';
     <div class="col-lg-7 col-md-7 ml-2">
         <div class="contenedor-carrusel">
             <div class="imagen-carrusel actual-carrusel">
-                <img src="https://i.pinimg.com/originals/8f/02/ca/8f02caef344b4087f629a0e67adca5eb.jpg" />
+                <img class="imagen_disenador" src="https://i.pinimg.com/originals/8f/02/ca/8f02caef344b4087f629a0e67adca5eb.jpg" />
             </div>
 
             <div class="imagen-carrusel">
-                <img src="https://i.pinimg.com/originals/64/fe/11/64fe11a394d91aca3eb8443095929cbd.jpg" />
+                <img class="imagen_disenador" src="https://i.pinimg.com/originals/64/fe/11/64fe11a394d91aca3eb8443095929cbd.jpg" />
             </div>
 
             <div class="imagen-carrusel">
-                <img src="https://townsquare.media/site/366/files/2019/11/judads-priest-singer-rob-halford-performs-during-firepower-2019-tour.jpg" />
+                <img class="imagen_disenador" src="https://townsquare.media/site/366/files/2019/11/judads-priest-singer-rob-halford-performs-during-firepower-2019-tour.jpg" />
             </div>
 
             <div class="imagen-carrusel">
-                <img src="https://www.rockalavena.cl/wp-content/uploads/2019/03/rob.jpg" />
+                <img class="imagen_disenador" src="https://www.rockalavena.cl/wp-content/uploads/2019/03/rob.jpg" />
             </div>
 
 
