cdb916936093eda2215c14e148d7800f2e064270
Cesh399 <Cesh399@users.noreply.github.com>
2020-08-09 20:49:17 -0400

    CRUD clientes listo

--- admin/consult_clients.php
+++ admin/consult_clients.php
@@ -9,7 +9,7 @@
 include '../config.php';
 
 //error_reporting(0);	//EVITAR MUESTRA DE ERRORES
-header('Content-type: application/json; charset=utf-8');    //ESTABLECE LA PAGINA COMO UN VISOR DE JSON
+//header('Content-type: application/json; charset=utf-8');    //ESTABLECE LA PAGINA COMO UN VISOR DE JSON
 
 define('NO_CONTENT_FOUND', 'No se se han encontrado resultados.');
 
@@ -18,9 +18,11 @@ $cedula = isset($_GET['cedula']) ? $_GET['cedula'] : '';
 $nombre = isset($_GET['nombre']) ? $_GET['nombre'] : '';
 $apellido = isset($_GET['apellido']) ? $_GET['apellido'] : '';
 $correo = isset($_GET['correo']) ? $_GET['correo'] : '';
-$codigo_de_area = isset($_GET['codigo_de_area']) ? $_GET['codigo_de_area'] : '';
-$numero = isset($_GET['numero']) ? $_GET['numero'] : '';
+$codigo_de_area = isset($_GET['codigoarea']) ? $_GET['codigoarea'] : '';
+$numero = isset($_GET['telefono']) ? $_GET['telefono'] : '';
 $usuario = isset($_GET['usuario']) ? $_GET['usuario'] : '';
+$id_usuario = isset($_GET['id_usuario']) ? $_GET['id_usuario'] : '';
+$id_telefono = isset($_GET['id_telefono']) ? $_GET['id_telefono'] : '';
 $estado = isset($_GET['estado']) ? $_GET['estado'] : '';
 $municipio = isset($_GET['municipio']) ? $_GET['municipio'] : '';
 $parroquia = isset($_GET['parroquia']) ? $_GET['parroquia'] : '';
@@ -40,15 +42,13 @@ if ($answer == '') {
     $connect = new AdminSQL();
 
     if($option === 'update') {
-      //$answer1 = $connect->update_notary($id, $name, $fklugar);
-      //$answer2 = $connect->update_coordinates($coordID, $latitud, $longitud);
-      //$answer3 = $connect->update_person($personID, $personname1, $personname2);
-      //$answer = [$answer1, $answer2, $answer3];
-
-    }elseif($option === 'delete'){
-      //$answer = $connect->delete_notary($id, $coordID, $personID);
+      $answer1 = $connect->update_client($cedula, $nombre, $apellido, $correo, $codigo_de_area, $numero, $id_telefono, $usuario, $id_usuario, $parroquia);
+    }else if($option === 'delete'){
+      $answer = $connect->delete_client($cedula, $id_usuario);
     }else if($option === 'select'){
       $answer = $connect->get_users_and_persons();
+    } else {
+      $answer = $connect->add_client($cedula, $nombre, $apellido, $correo, $codigo_de_area, $numero, $usuario, $parroquia);
     }
 
   } catch (PDOException $e) {
